a7f03eabd209497fe3a9ca199bfda4f8b113589e
Louis Turpinat <turpinat.louis@gmail.com>
2017-08-23 02:09:17 +0200

    Fix minor error bug

--- GestionCaisse MVVM/Converters/SellsScoreToSmileyImageConverter.cs
+++ GestionCaisse MVVM/Converters/SellsScoreToSmileyImageConverter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,18 +14,24 @@ namespace GestionCaisse_MVVM.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            var convertedValue = System.Convert.ToInt32(value);
+            try
+            {
+                var convertedValue = System.Convert.ToInt32(value);
 
-            string smiley = "";
-            if (convertedValue >= 15) smiley = "in-love";
-            else if (convertedValue >= 10 && convertedValue < 15) smiley = "really-happy";
-            else if (convertedValue >= 5 && convertedValue < 10) smiley = "very-happy";
-            else if (convertedValue >= 2 && convertedValue < 5) smiley = "really-happy";
-            else smiley = "happy";
+                string smiley = "";
+                if (convertedValue >= 15) smiley = "in-love";
+                else if (convertedValue >= 10 && convertedValue < 15) smiley = "really-happy";
+                else if (convertedValue >= 5 && convertedValue < 10) smiley = "very-happy";
+                else if (convertedValue >= 2 && convertedValue < 5) smiley = "really-happy";
+                else smiley = "happy";
+
+                var image = new BitmapImage(new Uri($"pack://application:,,,/Assets/smileys/{smiley}.png"));
+
+                return image;
+            }
+            catch { }
             
-            var image = new BitmapImage(new Uri($"pack://application:,,,/Assets/smileys/{smiley}.png"));
-
-            return image;
+            return new BitmapImage();
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
