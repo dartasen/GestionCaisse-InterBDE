907f4561e99c8b02b6841239296fc807bca278c2
Louis Turpinat <turpinat.louis@gmail.com>
2017-11-20 00:21:24 +0100

    New version

--- GestionCaisse MVVM/Model/Services/AppInformations.cs
+++ GestionCaisse MVVM/Model/Services/AppInformations.cs
@@ -2,7 +2,7 @@
 {
     public class AppInformations
     {
-        public static readonly string Version = "1.9";
+        public static readonly string Version = "2.2";
         public static readonly int DefaultSessionDelay = 300; //in seconds
         public static readonly int DefaultSessionDelayForSuperusers = 900; //in seconds
     }
